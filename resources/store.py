b948623d833a63514fc4874aa022bebdd5f74856
aloha-fim <frederec.im@gmail.com>
2023-04-10 03:05:56 -1000

    refactor schema with decorators to flask-smorest

--- resources/store.py
+++ resources/store.py
@@ -5,10 +5,13 @@ from flask_smorest import Blueprint, abort
 from db import stores
 from schemas import StoreSchema
 
+
 blp = Blueprint("stores", __name__, description="Operations on stores")
+
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+    @blp.response(200, StoreSchema)
     def get(self, store_id):
         try:
             return stores[store_id]
@@ -23,14 +26,14 @@ class Store(MethodView):
         except KeyError:
             abort(404, message="Store not found.")
 
-
-
 @blp.route("/store")
 class StoreList(MethodView):
+    @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return {"stores": list(stores.values())}
+        return stores.values()
     
     @blp.arguments(StoreSchema)
+    @blp.response(200, StoreSchema)
     def post(self, store_data):
         for store in store.values():
             if store_data["name"] == store["name"]:
