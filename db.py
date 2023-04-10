45796a05f8512d78d8b67d9a936dfcba90a72c46
aloha-fim <frederec.im@gmail.com>
2023-04-10 03:19:17 -1000

    sqlalchemy model setup

--- db.py
+++ db.py
@@ -1,13 +1,5 @@
+from flask_sqlalchemy import SQLAlchemy
 
-stores = {}
-items = {
-    1: {
-        "name":"Chair",
-        "price": 15.99
-    },
-    2: {
-        "name": "Table",
-        "price": 180.50
-    }
-}    
-    
+db = SQLAlchemy()
+
+
