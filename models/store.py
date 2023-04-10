c07625fda8550b261e8f7c06b4903e1fea401406
aloha-fim <frederec.im@gmail.com>
2023-04-10 03:32:43 -1000

    adding relationships to model schemas

--- models/store.py
+++ models/store.py
@@ -6,4 +6,4 @@ class StoreModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
-    
+    items = db.relationship("ItemModel", back_populates="store", lazy="dynamic")
