0fe9557ccc787ce6047afd9ab54c726dd36b8bfe
aloha-fim <frederec.im@gmail.com>
2023-04-11 19:04:40 -1000

    add tags blueprint and fix stores route

--- models/store.py
+++ models/store.py
@@ -7,3 +7,4 @@ class StoreModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
     items = db.relationship("ItemModel", back_populates="store", lazy="dynamic", cascade="all, delete")
+    tags = db.relationship("TagModel", back_populates="store", lazy="dynamic")
