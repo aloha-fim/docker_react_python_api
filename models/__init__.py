1ff0700e95a3b4c8d4600703d3a1182dc1f96867
aloha-fim <frederec.im@gmail.com>
2023-04-24 08:00:36 -1000

    add movies and ratings tables to models

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,5 @@ from models.item import ItemModel
 from models.tag import TagModel
 from models.item_tags import ItemTags
 from models.user import UserModel
+from models.movie import MovieModel
+from models.rating import RatingModel
---
+++ models/rating.py
@@ -0,0 +1,11 @@
+from db import db
+
+
+class RatingModel(db.Model):
+    __tablename__ = "ratings"
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey("movies.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    stars = db.Column(db.Integer)
