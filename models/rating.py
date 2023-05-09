2a867c7522a25c78dedf8c1b62fcf58129a9ceee
aloha-fim <frederec.im@gmail.com>
2023-05-09 16:18:08 -0400

    fix error to lazy config on one to one relationship

--- models/rating.py
+++ models/rating.py
@@ -9,4 +9,4 @@ class RatingModel(db.Model):
     movie_id = db.Column(db.Integer, db.ForeignKey("movies.id"))
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     stars = db.Column(db.Integer)
-    movie = db.relationship("MovieModel", back_populates="ratings", lazy="dynamic")
+    movie = db.relationship("MovieModel", back_populates="ratings")
