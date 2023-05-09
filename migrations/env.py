26792f2f2c38759851963b3e6c9dfc9d7a7fba2f
aloha-fim <frederec.im@gmail.com>
2023-05-09 15:14:55 -0400

    update models nameerror items_tags

--- migrations/env.py
+++ migrations/env.py
@@ -67,7 +67,7 @@ def run_migrations_offline():
     context.configure(
         url=url, 
         target_metadata=get_metadata(),
-        compare_type=True, 
+        #compare_type=True, 
         literal_binds=True
     )
 
@@ -100,7 +100,7 @@ def run_migrations_online():
             connection=connection,
             target_metadata=get_metadata(),
             process_revision_directives=process_revision_directives,
-            compare_type=True,
+            #compare_type=True,
             **current_app.extensions['migrate'].configure_args
         )
 
